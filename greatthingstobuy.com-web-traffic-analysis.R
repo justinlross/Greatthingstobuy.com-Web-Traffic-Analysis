99460dab9992c94482c60749e0a29c171d0162b3
Justin L Ross <37995778+justinlross@users.noreply.github.com>
2019-07-31 09:55:12 +1000

    Add files via upload

--- greatthingstobuy.com-web-traffic-analysis.R
+++ greatthingstobuy.com-web-traffic-analysis.R
@@ -8,17 +8,19 @@ library(ggplot2) # THE data visualization library for R
 #Declaring variables Section
 
 amazonOrdersData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Amazon Fee Orders Data.csv", header=TRUE, skip = 1, sep=",")
-#googleAcquisitionData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Google Acquisition.csv", header=TRUE, sep=",")
+googleAcquisitionData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Google Acquisition.csv", header=TRUE, sep=",")
 #googleAcquisitionSourceData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Google Acquisition Source.csv", header=TRUE, sep=",")
 googleCityData <- read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Google City.csv", header=TRUE, sep=",")
-#googleCountryData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Amazon Fee Orders Data.csv", header=TRUE, sep=",")
-
-
+googleCountryData <-read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Amazon Fee Orders Data.csv", header=TRUE, skip = 1, sep=",")
+#
+#
+googleNetworkReferrals <- read.table(file="C:/Users/Justin/Desktop/Datasets/Social Media Traffic Analysis/Google Network Referrals.csv", header=TRUE, sep=",")
 
 
 # Display output Section
 
 
+print(googleNetworkReferrals)
 
-print(googleCityData)
-print(amazonOrdersData)
+#print(googleCityData)
+#print(amazonOrdersData)
